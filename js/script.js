0ca552dadd0b6a541448756054936db4fd85cc20
lucaswotta <lucaswotta@gmail.com>
2022-10-19 02:36:35 -0300

    Add vanilla tilt

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.container'),
     qrInput = container.querySelector('.form input'),
     generateBtn = container.querySelector('.form button'),
     qrImg = container.querySelector('.qr-code .img');
-    
+
 generateBtn.addEventListener('click', () => {
     let qrValue = qrInput.value;
     if (!qrValue) {
