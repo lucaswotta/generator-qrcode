604b768718b6e9981289c5d1d08fbeb14ff56bad
lucaswotta <lucaswotta@gmail.com>
2022-10-18 23:56:23 -0300

    Add keypress enter

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.container'),
+    qrInput = container.querySelector('.form input'),
+    generateBtn = container.querySelector('.form button'),
+    qrImg = container.querySelector('.qr-code .img');
+    
+generateBtn.addEventListener('click', () => {
+    let qrValue = qrInput.value;
+    if (!qrValue) {
+        return;
+    }
+    generateBtn.innerText = "Gerando...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.addEventListener('load', () => {
+        generateBtn.innerText = "Gerar";
+        container.classList.add('active');
+    });
+});
+
+qrInput.addEventListener('keyup', () => {
+    if (!qrInput.value) {
+        container.classList.remove('active');
+    };
+});
+
+document.addEventListener('keypress', function (e) {
+    if (e.key === "Enter") {
+        generateBtn.click();
+    }
+});
